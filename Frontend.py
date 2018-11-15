7579735f8feead525a84881e93302779f4cdfb75
PatrickNorton <patrick.147.norton@gmail.com>
2018-11-15 08:44:35 -0500

    Changed logo to be the .gif from .jpg

--- Frontend.py
+++ Frontend.py
@@ -1,7 +1,7 @@
 # Plz use this to create a frontend
 import tkinter as tk
 root = tk.Tk()
-logo = tk.PhotoImage(file='Monopolyboard.jpg')
+logo = tk.PhotoImage(file='Monopolyboard.gif')
 logo = logo.zoom(2)
 logo = logo.subsample(5)
 w = tk.Label(root, image=logo)
